ae06f327617c250d731a0ffbef032a1244424b56
Embers-of-the-Fire <stellarishs@163.com>
2024-03-09 12:04:19 +0800

    add: add `force_assert` feature

--- ucsi-test/tests/test_ops.rs
+++ ucsi-test/tests/test_ops.rs
@@ -1,4 +1,5 @@
 #![feature(type_alias_impl_trait)]
+#![allow(unused_variables)]
 
 use std::error::Error;
 
@@ -11,22 +12,24 @@ use ucsi::{
         value::Value,
     },
     macros::cast::{cast_si_value, infer_cast_si_value},
-    unit, units::any::{SiOpsUnit, CastFrom},
+    unit, units::any::{SiOpsUnit, CastFrom}, val,
 };
 
 #[test]
 fn test_ucsi_ops() {
-    let speed: Value<f64, unit!(m / s)> = Value::new(10.0);
-    let time: Value<f64, s> = Value::new(1.0);
+    let speed = val!(10.0 * (m / s));
+    let time = val!(1.0 * s);
     let acc = speed / time;
-    let mass: Value<f64, kg> = Value::new(1.0);
+    let mass = val!(1.0 * kg);
     let force = acc * mass;
     let checked_force = cast_si_value!(force => unit!(((m / s) / s) * kg) as Newton);
-    println!("{}", checked_force.display_si());
+    // println!("{}", checked_force.display_si().unwrap());
     let checked_force = infer_cast_si_value!(force => Newton);
-    println!("{}", checked_force.display_si());
+    // println!("{}", checked_force.display_si().unwrap());
     let checked_force = force.cast_const::<Newton>();
-    println!("{}", checked_force.display_si());
+    // println!("{}", checked_force.display_si().unwrap());
+    // println!("{}", checked_force.display_si_latex().unwrap());
+    // println!("{}", checked_force.display_si_symbol_latex().unwrap());
 }
 
 #[test]
@@ -41,7 +44,7 @@ fn test_cast_ops() {
     let mass: Value<f64, kg> = Value::new(1.0);
     let force = acc * mass;
     let checked_force: Value<f64, s> = can_cast(force);
-    println!("{}", checked_force.display_si());
+    // println!("{}", checked_force.display_si().unwrap());
 }
 
 #[test]
@@ -51,7 +54,7 @@ fn test_ops_display() -> Result<(), Box<dyn Error>> {
     let acc = speed / time;
     let mass: Value<f64, kg> = Value::new(1.0);
     let force = acc * mass;
-    let dis = force.display_symbol()?;
-    println!("{}", dis);
+    // let dis = force.display_symbol()?;
+    // println!("{}", dis);
     Ok(())
 }
